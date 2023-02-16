884fb36a48d39291ba071dabf0fd0fc3d213a2ce
Troy Sankey <tsankey@edx.org>
2023-02-16 12:48:26 -0500

    chore: bump version

--- openedx_ledger/__init__.py
+++ openedx_ledger/__init__.py
@@ -1,6 +1,6 @@
 """
 A library that records transactions against a ledger, denominated in units of value.
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 default_app_config = "openedx_ledger.apps.EdxLedgerConfig"
