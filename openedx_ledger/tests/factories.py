7d16dc6055c40beaf9f27a11086ebc834e9ec98a
Troy Sankey <tsankey@edx.org>
2023-03-02 20:59:20 -0500

    feat: Add test factories
    And bump version.
    
    ENT-6836

---
+++ openedx_ledger/tests/factories.py
@@ -0,0 +1,38 @@
+"""
+Test factories for openedx-ledger models.
+"""
+from uuid import uuid4
+
+import factory
+
+from openedx_ledger.models import Ledger, Transaction, UnitChoices
+
+
+class LedgerFactory(factory.django.DjangoModelFactory):
+    """
+    Test factory for the `Ledger` model.
+
+    By default, no transactions are created for this test Ledger.
+    """
+    class Meta:
+        model = Ledger
+
+    uuid = factory.LazyFunction(uuid4)
+    idempotency_key = factory.Faker("lexify", text="subsidy-????")
+    unit = UnitChoices.USD_CENTS
+
+
+class TransactionFactory(factory.django.DjangoModelFactory):
+    """
+    Test factory for the `Transaction` model.
+    """
+    class Meta:
+        model = Transaction
+
+    uuid = factory.LazyFunction(uuid4)
+    idempotency_key = factory.Faker(
+        "lexify",
+        text=factory.LazyAttribute(lambda tx: f"{tx.ledger.idempotency_key}-{tx.quantity}-????"),
+    )
+    quantity = factory.Faker("random_int", min=-100000, max=-100)
+    ledger = factory.Iterator(Ledger.objects.all())
