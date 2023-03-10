ba43fb1cbf8aa449fd618e87d5f5369d36a44976
Alex Dusenbery <adusenbery@edx.org>
2023-03-10 16:35:46 -0500

    feat: ledger initialization stuff in django admin

---
+++ openedx_ledger/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants for the openedx_ledger app.
+"""
+LEDGER_DEFAULT_IDEMPOTENCY_KEY_PREFIX = 'ledger-default'
+LEDGERED_SUBSIDY_IDEMPOTENCY_KEY_PREFIX = 'ledger-for-subsidy'
+
+INITIAL_DEPOSIT_TRANSACTION_SLUG = 'initial-deposit'
