151d138fd1a42879b1b0cdb05f92d86833c31f29
Troy Sankey <tsankey@edx.org>
2023-03-13 16:32:43 -0400

    chore: bump version to 0.1.7
    I forgot to bump the version in the last PR.
    
    ENT-6803 cont.

--- openedx_ledger/__init__.py
+++ openedx_ledger/__init__.py
@@ -1,6 +1,6 @@
 """
 A library that records transactions against a ledger, denominated in units of value.
 """
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 default_app_config = "openedx_ledger.apps.EdxLedgerConfig"
