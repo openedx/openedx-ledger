fa951f83733c6fa1bdd88ded0fdc95abaff1fb8e
John Nagro <jnagro@edx.org>
2023-05-01 14:13:28 +0000

    fix: drop provider name constraints

--- openedx_ledger/__init__.py
+++ openedx_ledger/__init__.py
@@ -1,6 +1,6 @@
 """
 A library that records transactions against a ledger, denominated in units of value.
 """
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 default_app_config = "openedx_ledger.apps.EdxLedgerConfig"
