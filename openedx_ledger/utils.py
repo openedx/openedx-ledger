67315cb9ed1e9301d0dfed750cb573375e754fd2
Troy Sankey <tsankey@2u.com>
2024-07-03 12:10:40 -0700

    feat: add util function to find "legacy" initial deposits
    This will be in support of a future data migration or management
    command to backfill deposits for initial transactions that are missing
    a deposit.
    
    ENT-9075

--- openedx_ledger/utils.py
+++ openedx_ledger/utils.py
@@ -4,6 +4,11 @@ Common utility functions that don't create/update/destroy business objects.
 import hashlib
 from uuid import uuid4
 
+from django.db.models import F, Window
+from django.db.models.functions import FirstValue
+
+from openedx_ledger.models import Transaction
+
 from .constants import (
     INITIAL_DEPOSIT_TRANSACTION_SLUG,
     LEDGER_DEFAULT_IDEMPOTENCY_KEY_PREFIX,
@@ -46,3 +51,35 @@ def create_idempotency_key_for_transaction(ledger, quantity, is_initial_deposit=
         }
     hashed_metadata = hashlib.md5(str(idpk_data).encode()).hexdigest()
     return f'{ledger.idempotency_key}-{quantity}-{hashed_metadata}'
+
+
+def find_legacy_initial_transactions():
+    """
+    Heuristic to identify "legacy" initial transactions.
+
+    An initial transaction is one that has the following traits:
+    * Is chronologically the first transaction for a Ledger.
+    * Contains a hint in its idempotency key which indicates that it is an initial deposit.
+    * Has a positive quantity.
+
+    A legacy initial transaction is one that has the following additional traits:
+    * does not have a related Deposit.
+    """
+    # All transactions which are chronologically the first in their respective ledgers.
+    first_transactions = Transaction.objects.annotate(
+        first_tx_uuid=Window(
+            expression=FirstValue('uuid'),
+            partition_by=['ledger'],
+            order_by=F('created').asc(),  # "first chronologically" above means first created.
+        ),
+    ).filter(uuid=F('first_tx_uuid'))
+
+    # Further filter first_transactions to find ones that qualify as _initial_ and _legacy_.
+    legacy_initial_transactions = first_transactions.filter(
+        # Traits of an _initial_ deposit:
+        idempotency_key__contains=INITIAL_DEPOSIT_TRANSACTION_SLUG,
+        quantity__gte=0,
+        # Traits of a _legacy_ initial deposit:
+        deposit__isnull=True,
+    )
+    return legacy_initial_transactions
